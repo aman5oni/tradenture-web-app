45b4cf36dcac1fdcc1b71562fb9cf5de11c7db82
Aman Soni <66624836+aman5oni@users.noreply.github.com>
2022-04-30 14:35:20 +0530

    ""

--- tradenture-app/src/Assets/Components/Header/Header.js
+++ tradenture-app/src/Assets/Components/Header/Header.js
@@ -3,7 +3,17 @@ import "./Header.css"
 
 function MyHeader() {
   return (
-    <div className="headerMain">MyHeaderSection</div>
+    <div className="headerMain">
+        <div className="mainLogo">
+            Logo
+        </div>
+        <div className='searchBar'>
+            Search
+        </div>
+        <div className='loginButton'>
+            Login
+        </div>
+    </div>
   )
 }
 
