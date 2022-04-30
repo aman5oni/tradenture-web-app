4f84eef007f5a14bd123cdcf48f1035def9f7103
Aman Soni <66624836+aman5oni@users.noreply.github.com>
2022-04-30 12:33:42 +0530

    "Working Commit"

---
+++ tradenture-app/src/Manage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Login  from './Assets/Authentication/Login';
+
+export default function componentName() {
+  return (
+    <div>
+        <Login/>
+    </div>
+  );
+}
